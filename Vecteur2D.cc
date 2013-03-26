93aef2050e003d5e47391c9efcc4240e5969308e
Guillaume Leclerc <guillaume.leclerc.work@gmail.com>
2013-03-26 23:29:53 +0100

    Remise en forme du code

--- Vecteur2D.cc
+++ Vecteur2D.cc
@@ -3,122 +3,93 @@
 #include "Vecteur2D.h"
 using namespace std;
 
+void Vecteur2D::affiche() 
+{
+	cout <<dim1<<" "<<dim2<< endl;
+}
 
-void Vecteur2D::affiche() {cout <<dim1<<" "<<dim2<< endl;}
+
 bool Vecteur2D::compare(Vecteur2D x)
-    {
-        if ((x.dim1==dim1) & (x.dim2==dim2))
-            {
-                cout <<"true"<<endl; return(true);
-            }
-
-        else
-            {
-                cout <<"false"<<endl; return (false);
-            }
-    } // opérateur compare
-
-
-
-
-	/* en gros eric, le concept est d'utiliser une methode qui va etre applée en tant que caractéristique d'un type d'objet
-   donc on va appeler la methode via la classe en prenant des arguments des objets externes: exemple pour
-   l'addition on a : A.addition(D) qui va faire l'addition entre le vecteur A et le vecteur D c'est pareil que de faire D.addition(A);
-   Si tu comprends pas reli ton cour !
-   Dans chaque methodes les vecteurs hotes de la methodes (vecteurs que on va retourner) sont només des facon logique ex: somm.....
-   */
-	/*void affiche ()
-{cout <<dim1<<" "<<dim2<< endl;};
-
-	bool compare (Vecteur2D x)
-{if ((x.dim1==dim1) & (x.dim2==dim2)) {cout <<"true"<<endl; return(true);}
-
-else {cout <<"false"<<endl; return (false);};
-
-};*/
-    //opé addition
-	Vecteur2D Vecteur2D::operator+(Vecteur2D autre) const
+{
+	if ((x.dim1==dim1) & (x.dim2==dim2))
 	{
-
-		Vecteur2D somm;
-
-		somm.dim1=dim1+autre.dim1;
-		somm.dim2=dim2+autre.dim2;
-
-		return somm;
-
+		cout <<"true"<<endl; return(true);
 	}
+
+	else
+	{
+		cout <<"false"<<endl; return (false);
+	}
+} // opérateur compare
+
+
+//opé addition
+Vecteur2D Vecteur2D::operator+(Vecteur2D autre) const
+{
+
+	Vecteur2D somm;
+
+	somm.dim1=dim1+autre.dim1;
+	somm.dim2=dim2+autre.dim2;
+
+	return somm;
+
+}
 //opé soustraction
-	Vecteur2D Vecteur2D::operator-(Vecteur2D autres) const
-	{
-		Vecteur2D sous;
+Vecteur2D Vecteur2D::operator-(Vecteur2D autres) const
+{
+	Vecteur2D sous;
 
-	  sous.dim1=dim1-autres.dim1;
-	  sous.dim2=dim2-autres.dim2;
+	sous.dim1=dim1-autres.dim1;
+	sous.dim2=dim2-autres.dim2;
 
-	  return sous ;
-
-
-
-
-	}
+	return sous ;
+}
 //opé opposé
-	Vecteur2D Vecteur2D::operator-=(Vecteur2D opp) const
-	{
+Vecteur2D Vecteur2D::operator-=(Vecteur2D opp) const
+{
 
-		opp.dim1=-dim1;
-		opp.dim2=-dim2;
+	opp.dim1=-dim1;
+	opp.dim2=-dim2;
 
-		return opp;
+	return opp;
 
-	}
+}
 //opé prod par un scalaire
-	Vecteur2D Vecteur2D::operator*(double x)
-	{
-		Vecteur2D mult;
-		mult.dim1=dim1*x ;
-		mult.dim2=dim2*x;
+Vecteur2D Vecteur2D::operator*(double x)
+{
+	Vecteur2D mult;
+	mult.dim1=dim1*x ;
+	mult.dim2=dim2*x;
 
 	return mult;
 
-	}
+}
 //Opé normesquare
-	Vecteur2D::operator++(double x)
-	{
-
-
-
+Vecteur2D::operator++ (double x)
+{
 	x=dim1*dim1+dim2*dim2;
-
 	return x;
-
-	}
+}
 
 //opé norme
-	Vecteur2D::operator|| (double x)
-	{
-
-
-
+Vecteur2D::operator|| (double x)
+{
 	x=sqrt(dim1*dim1+dim2*dim2);
 
 	return x;
-
-
-
-
-	}
+}
 // opé prod scalaire
-	Vecteur2D::operator->* (Vecteur2D autrev)
-	{ double x;
+Vecteur2D::operator->* (Vecteur2D autrev)
+{
+	double x;
 	Vecteur2D v3;
 	v3=addition(autrev);
-		x=(1/2)*(autrev.normesquare()+normesquare()-v3.normesquare() );
+	x=(1/2)*(autrev.normesquare()+normesquare()-v3.normesquare() );
 
 	return x;
 
-
-	}
+}
 };
 
 double Vecteur2D :: getDim1() const { return dim1; }
@@ -145,85 +116,85 @@ void Vecteur2D affiche() {cout <<dim1<<" "<<dim2<< endl;} // opérateur affiche
 
 
 bool Vecteur2D compare(Vecteur2D x)
-    {
-        if ((x.dim1==dim1) & (x.dim2==dim2))
-            {
-                cout <<"true"<<endl; return(true);
-            }
+{
+	if ((x.dim1==dim1) & (x.dim2==dim2))
+	{
+		cout <<"true"<<endl; return(true);
+	}
 
-        else
-            {
-                cout <<"false"<<endl; return (false);
-            }
-    } // opérateur compare
-
-
+	else
+	{
+		cout <<"false"<<endl; return (false);
+	}
+} // opérateur compare
 
 
-	/* en gros eric, le concept est d'utiliser une methode qui va etre applée en tant que caractéristique d'un type d'objet
-   donc on va appeler la methode via la classe en prenant des arguments des objets externes: exemple pour
-   l'addition on a : A.addition(D) qui va faire l'addition entre le vecteur A et le vecteur D c'est pareil que de faire D.addition(A);
-   Si tu comprends pas reli ton cour !
-   Dans chaque methodes les vecteurs hotes de la methodes (vecteurs que on va retourner) sont només des facon logique ex: somm.....
-   */
-	/*void affiche ()
+
+
+/* en gros eric, le concept est d'utiliser une methode qui va etre applée en tant que caractéristique d'un type d'objet
+donc on va appeler la methode via la classe en prenant des arguments des objets externes: exemple pour
+l'addition on a : A.addition(D) qui va faire l'addition entre le vecteur A et le vecteur D c'est pareil que de faire D.addition(A);
+Si tu comprends pas reli ton cour !
+Dans chaque methodes les vecteurs hotes de la methodes (vecteurs que on va retourner) sont només des facon logique ex: somm.....
+*/
+/*void affiche ()
 {cout <<dim1<<" "<<dim2<< endl;};
 
-	bool compare (Vecteur2D x)
+bool compare (Vecteur2D x)
 {if ((x.dim1==dim1) & (x.dim2==dim2)) {cout <<"true"<<endl; return(true);}
 
 else {cout <<"false"<<endl; return (false);};
 
 };*/
 
-	Vecteur2D Vecteur2D::operator addition(Vecteur2D autre) const
-	{
+Vecteur2D Vecteur2D::operator addition(Vecteur2D autre) const
+{
 
-		Vecteur2D somm;
+	Vecteur2D somm;
 
-		somm.dim1=dim1+autre.dim1;
-		somm.dim2=dim2+autre.dim2;
+	somm.dim1=dim1+autre.dim1;
+	somm.dim2=dim2+autre.dim2;
 
-		return somm;
+	return somm;
 
-	}
+}
 
-	Vecteur2D Vecteur2D::operator soustraction(Vecteur2D autres) const
-	{
-		Vecteur2D sous;
+Vecteur2D Vecteur2D::operator soustraction(Vecteur2D autres) const
+{
+	Vecteur2D sous;
 
-	  sous.dim1=dim1-autres.dim1;
-	  sous.dim2=dim2-autres.dim2;
+	sous.dim1=dim1-autres.dim1;
+	sous.dim2=dim2-autres.dim2;
 
-	  return sous ;
-
-
+	return sous ;
 
 
-	}
 
-	Vecteur2D Vecteur2D::operator oppose() const
-	{
-		Vecteur2D opp;
-		opp.dim1=-dim1;
-		opp.dim2=-dim2;
 
-		return opp;
+}
 
-	}
+Vecteur2D Vecteur2D::operator oppose() const
+{
+	Vecteur2D opp;
+	opp.dim1=-dim1;
+	opp.dim2=-dim2;
 
-	Vecteur2D Vecteur2D::operator multiscal (double x)
-	{
-		Vecteur2D mult;
-		mult.dim1=dim1*x ;
-		mult.dim2=dim2*x;
+	return opp;
+
+}
+
+Vecteur2D Vecteur2D::operator multiscal (double x)
+{
+	Vecteur2D mult;
+	mult.dim1=dim1*x ;
+	mult.dim2=dim2*x;
 
 	return mult;
 
-	}
+}
 
-	Vecteur2D::operator double normesquare ()
-	{
+Vecteur2D::operator double normesquare ()
+{
 
 	double x;
 
@@ -231,10 +202,10 @@ else {cout <<"false"<<endl; return (false);};
 
 	return x;
 
-	}
+}
 
-	Vecteur2D::operator double norme ()
-	{
+Vecteur2D::operator double norme ()
+{
 
 	double x;
 
@@ -245,18 +216,19 @@ else {cout <<"false"<<endl; return (false);};
 
 
 
-	}
+}
 
-	Vecteur2D::operator double prodscal (Vecteur2D autrev)
-	{ double x;
+Vecteur2D::operator double prodscal (Vecteur2D autrev)
+{
+	double x;
 	Vecteur2D v3;
 	v3=addition(autrev);
-		x=(1/2)*(autrev.normesquare()+normesquare()-v3.normesquare() );
+	x=(1/2)*(autrev.normesquare()+normesquare()-v3.normesquare() );
 
 	return x;
 
 
-	}
+}
 }
 
 double Vecteur2D :: getDim1() const { return dim1; };
