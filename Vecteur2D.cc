1147b3992f6cf07ea2ba88f84e82c1721660e88e
Paic eric <eric.paic@gmail.com>
2013-03-26 23:32:52 +0100

    Suppression methode en double

--- Vecteur2D.cc
+++ Vecteur2D.cc
@@ -3,7 +3,7 @@
 #include "Vecteur2D.h"
 using namespace std;
 
-void Vecteur2D::affiche() 
+void Vecteur2D::affiche()
 {
 	cout <<dim1<<" "<<dim2<< endl;
 }
@@ -146,99 +146,4 @@ bool compare (Vecteur2D x)
 else {cout <<"false"<<endl; return (false);};
 
 };*/
-
-Vecteur2D Vecteur2D::operator addition(Vecteur2D autre) const
-{
-
-	Vecteur2D somm;
-
-	somm.dim1=dim1+autre.dim1;
-	somm.dim2=dim2+autre.dim2;
-
-	return somm;
-
-}
-
-Vecteur2D Vecteur2D::operator soustraction(Vecteur2D autres) const
-{
-	Vecteur2D sous;
-
-	sous.dim1=dim1-autres.dim1;
-	sous.dim2=dim2-autres.dim2;
-
-	return sous ;
-
-
-
-
-}
-
-Vecteur2D Vecteur2D::operator oppose() const
-{
-	Vecteur2D opp;
-	opp.dim1=-dim1;
-	opp.dim2=-dim2;
-
-	return opp;
-
-}
-
-Vecteur2D Vecteur2D::operator multiscal (double x)
-{
-	Vecteur2D mult;
-	mult.dim1=dim1*x ;
-	mult.dim2=dim2*x;
-
-	return mult;
-
-}
-
-Vecteur2D::operator double normesquare ()
-{
-
-	double x;
-
-	x=dim1*dim1+dim2*dim2;
-
-	return x;
-
-}
-
-Vecteur2D::operator double norme ()
-{
-
-	double x;
-
-	x=sqrt(dim1*dim1+dim2*dim2);
-
-	return x;
-
-
-
-
-}
-
-Vecteur2D::operator double prodscal (Vecteur2D autrev)
-{
-	double x;
-	Vecteur2D v3;
-	v3=addition(autrev);
-	x=(1/2)*(autrev.normesquare()+normesquare()-v3.normesquare() );
-
-	return x;
-
-
-}
-}
-
-double Vecteur2D :: getDim1() const { return dim1; };
-double Vecteur2D :: getDim2() const { return dim2; };
-void Vecteur2D :: setDim1(double x) { dim1 = x; };
-void Vecteur2D :: setDim2(double y) { dim2 = y; };
-
-
-
-/*le main sert juste a creer des test pour voir si la classe est bien faite
-*/
-
 
