223c40831c5f99c74d2cf241163cae12288f20f1
Paic eric <eric.paic@gmail.com>
2013-04-02 14:52:10 +0200

    Dessinable.h

--- Dessinable.h
+++ Dessinable.h
@@ -1,1 +1,13 @@
+#ifndef PRJ_DESSINABLE_H
+#define PRJ_DESSINABLE_H
+#include <iostream>
 
+using namespace std;
+
+
+class Dessinable
+{
+        virtual void dessine () const {}
+};
+
+// endif // PRJ_DESSINABLE_H
