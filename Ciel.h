f34cdaa546a8596e4ffee585d3f861e9e5ab1bba
stanislas aurain <stanislas.au@>
2013-04-02 15:01:17 +0200

    voila eric t content?

---
+++ Ciel.h
@@ -0,0 +1,36 @@
+#ifndef PRJ_CIEL_H
+#define PRJ_CIEL_H
+#include <iostream>
+#include <cmath>
+#include "CubedAir.h"
+using namespace std;
+
+class Ciel
+{
+	private :
+    int nbCubeX;
+    int nbCubeY;
+    int nbCubeZ;
+
+    vector <vector <vector <CubedAir>>> collectionCubedAir;
+
+
+    // Dimensions de la boite
+    double coordonneeX;
+    double coordonneeY;
+    double coordonneeZ;
+    double lambda; // pas
+
+     public :
+ void Ciel::coordonnee(double v, Champspotentiel C)
+ void Ciel::initialise(nbCubeX,nbCubeY,nbCubeZ,lambda,double v)
+
+
+
+
+}
+
+
+
+
+#endif PRJ_CIEL_H
---
+++ CubedAir.h
@@ -0,0 +1,31 @@
+#ifndef PRJ_CUBEDAIR_H
+#define PRJ_CUBEDAIR_H
+#include <iostream>
+#include <cmath>
+#include "Vecteur2D.h"
+
+using namespace std;
+
+class CubedAir 
+{
+private:
+	const double Mairsec(0.02896)
+	const double Gazparfait(8.3144621)
+	double Tempsol;
+	//*température au sol
+	double pressairsol;
+	//* pression de l'air au sol
+
+
+
+
+
+
+
+
+
+
+}
+
+
+#endif PRJ_CUBEDAIR_H
