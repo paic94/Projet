8e9838cef2086553be1e43607877ad45e4461ebf
Paic eric <eric.paic@gmail.com>
2013-04-02 18:23:02 +0200

    enlevage {}

--- Dessinable.h
+++ Dessinable.h
@@ -9,10 +9,10 @@ abstract class Dessinable
 {
         public :
 
-        virtual string Dessinable::get_string(){};
+        virtual string Dessinable::get_string();
 
-        void Dessinable::dessine () const {} // dessine est un virtuel pour la question 9.1
-        ostream& Dessinable::operator<<(ostream& out, Dessinable objetDessinable) {}
+        void Dessinable::dessine () const  // dessine est un virtuel pour la question 9.1
+        ostream& Dessinable::operator<<(ostream& out, Dessinable objetDessinable)
 };
 
 // endif // PRJ_DESSINABLE_H
